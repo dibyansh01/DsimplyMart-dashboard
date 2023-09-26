ee32988e6a78fb4fd8eb8b413e0b6744806ab87f
Dibyanshu <dibyansh01iitk@gmail.com>
2023-09-26 17:12:32 +0530

    added action in billboard

--- app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
@@ -1,6 +1,11 @@
 "use client"
+import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
+import { toast } from "react-hot-toast";
 
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { BillboardColomn } from "./columns"
+import { Button } from "@/components/ui/button";
+
 
 interface CellActionProps {
     data: BillboardColomn;
@@ -12,9 +17,38 @@ export const CellAction: React.FC<CellActionProps> = (
         data
     }
 )=>{
+    
+    const onCopy = (id: string) => {
+        navigator.clipboard.writeText(id);
+        toast.success("Billboard Id compied to the clipboard.")
+    }
+
     return (
-        <div>
-            Action
-        </div>
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="h-8 w-8 p-0">
+                    <span className="sr-only"> Open Menu </span>
+                    <MoreHorizontal  className="h-4 w-4"/>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+                <DropdownMenuLabel>
+                    Actions
+                </DropdownMenuLabel>
+                <DropdownMenuItem onClick={()=> onCopy(data.id)}>
+                    <Copy className="mr-2 h-4 w-4"/>
+                    Copy Id
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                    <Edit className="mr-2 h-4 w-4"/>
+                    Update
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                    <Trash className="mr-2 h-4 w-4"/>
+                    Delete
+                </DropdownMenuItem>
+
+            </DropdownMenuContent>
+        </DropdownMenu>
     )
 }
